5e1d97739073eab259fee5b8eedb6e49fc008d09
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-05 18:01:19 +0700

    commit 3

--- file.cpp
+++ file.cpp
@@ -3,3 +3,16 @@
 #include <iostream>
 using namespace std;
 
+// Node class representing a single node in the Linked List
+class Node 
+{
+public:
+    int data;
+    Node* next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
